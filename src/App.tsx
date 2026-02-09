bed47cb09082849d4373bb89f5dd6628db7cce99
Charles Campbell <crc00042@mix.wvu.edu>
2026-02-08 20:37:14 -0500

    add plasmic host route

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,15 @@ import HomePage from './pages/HomePage';
 import DisciplinePage from './pages/DisciplinePage';
 import QuestionDetailPage from './pages/QuestionDetailPage';
 import FeedbackPage from './pages/FeedbackPage';
+import PlasmicHostPage from './pages/PlasmicHostPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Plasmic host route: must NOT be under Layout */}
+        <Route path="/plasmic-host" element={<PlasmicHostPage />} />
+
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="feedback" element={<FeedbackPage />} />
@@ -21,3 +25,4 @@ function App() {
 }
 
 export default App;
+
